185464905b07fd683d584fe31bb669d8e6b9c61a
meowprd <meowprd@gmail.com>
2025-08-11 09:44:04 +0500

    feat(config,views): add Twig configuration and initialize views directory
    - Define VIEWS_PATH constant in app.php for Twig integration
    - Create Twig service setup with filesystem loader
    - Add .gitkeep to preserve empty views directory structure

--- config/app.php
+++ config/app.php
@@ -4,5 +4,5 @@ define('ROOT_PATH', dirname(__DIR__));
 $dotenv = new \Symfony\Component\Dotenv\Dotenv();
 $dotenv->load(ROOT_PATH . '/.env');
 
-
-define('API_PREFIX', '/api');
+const API_PREFIX = '/api';
+const VIEWS_PATH = ROOT_PATH . '/views';
---
+++ config/services/Twig.php
@@ -0,0 +1,9 @@
+<?php
+
+return function(League\Container\Container $container) {
+    $container->addShared('twig-filesystemloader', \Twig\Loader\FilesystemLoader::class)
+        ->addArgument(new \League\Container\Argument\Literal\StringArgument(VIEWS_PATH));
+
+    $container->addShared(\Twig\Environment::class)
+        ->addArgument('twig-filesystemloader');
+};
